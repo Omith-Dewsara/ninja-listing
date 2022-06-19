70c622c65aa56c07143d8e2d4b2009f3b5eaecb8
Omith Dewsara <OmithDewsara123@gmail.com>
2022-06-19 07:59:13 -0400

    'initial'

---
+++ Comps/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => (
+	<footer>
+		<p> Copyright 2021 Ninja list </p>
+	</footer>
+)
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const About = () => (
+	<>
+		<Head>
+			<title> Ninja List | About </title>
+			<meta name="keywords" content="ninjas" />
+		</Head>
+		<div>
+			<h1> About </h1>
+		</div>
+	</>
+)
+
+export default About;
